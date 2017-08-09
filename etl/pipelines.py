1b4e32d2bd5d55f9fd587814f80ec47a3398947c
Harrison Bond <baronvonbadguy@gmail.com>
2017-08-08 23:36:29 -0400

    Rework classes, insert titanic dataset into db as well

--- etl/pipelines.py
+++ etl/pipelines.py
@@ -1,55 +1,120 @@
-import luigi
-import urllib2
+import os
 import sqlite3
+from os import path as osp
+
+import luigi
 import pandas as pd
+import requests
 
 
-
-class extractData(luigi.Task):
-	"""
-	EXTRACT data from kaggle
-	"""
-
-	def output(self):
-		return luigi.LocalTarget("titanic.csv")
-
-
-	def run(self):
-		#TODO Add Anidata repo when merged
-		url = "https://raw.githubusercontent.com/agconti/kaggle-titanic/master/data/train.csv"
-		response = urllib2.urlopen(url)
-		with open("titanic.csv","wb") as f:
-			f.write(response.read())
+def abspath(filename):
+    '''Returns a path by appending the input filename to 
+    the directory in which this file resides.
+    
+    Parameters
+    ----------
+    filename : str
+    '''
+    return osp.join(osp.dirname(os.path.abspath(__file__)), filename)
 
 
+class ExtractData(luigi.Task):
+    '''
+    Extract data from Kaggle and write it to disk
+    
+    Parameters
+    ----------
+    data_source : str
+        The remote location of the Titanic data set from
+        the Kaggle competition
+    output_name : str
+        The local name of the dataset file
+    '''
 
-class transformData(luigi.Task):
-	"""
-	TRANSFORM data  by creating aggregates og classes
-	"""
-	def requires(self):
-        	return extractData()
+    data_source = luigi.Parameter(
+        default='https://raw.githubusercontent.com/agconti/'
+                'kaggle-titanic/master/data/train.csv'
+    )
+    output_name = luigi.Parameter(default='titanic.csv')
 
-	def output(self):
-        	return luigi.LocalTarget("agg.csv")
+    def output(self):
+        path = abspath(self.output_name)
+        return luigi.LocalTarget(path)
 
-	def run(self):
+    def run(self):
+        # TODO Add Anidata repo when merged
+        response = requests.get(self.data_source)
+        data = response.text
+        with self.output().open('w') as f:
+            f.write(data)
 
-		data = pd.read_csv("titanic.csv")
-		agg = data.groupby("Pclass")["Pclass"].count()
-		agg.to_csv("agg.csv")
 
-class loadData(luigi.Task):
-	"""
-	LOAD Data into sql server
-	"""
-	def requires(self):
-		return transformData()
+class TransformData(luigi.Task):
+    '''
+    Transform data by creating aggregate counts of ticket
+    classes
+    
+    Parameters
+    ----------
+    agg_output_name : str
+        The local name of the output aggregate data file
+    titanic_output_name : str
+        THe local name of the titanic dataset with modified
+        column names
+    '''
 
-	def run(self):
-		data = pd.read_csv("agg.csv")
-		conn = sqlite3.connect("database.sqlite")
-		data.to_sql("aggregate",con = conn,if_exists="replace")
-		conn.close()
+    agg_output_name = luigi.Parameter(default='aggregated.csv')
+    titanic_output_name = luigi.Parameter(default='titanic_renamed.csv')
+
+    def requires(self):
+        return ExtractData()
+
+    def output(self):
+        agg_path = abspath(self.agg_output_name)
+        tit_path = abspath(self.titanic_output_name)
+        return (luigi.LocalTarget(agg_path), luigi.LocalTarget(tit_path))
+
+    def run(self):
+        with self.input().open('r') as f:
+            data = pd.read_csv(f)
+
+        # lowercase all column names in prep for loading into database
+        data.columns = [col.lower() for col in data.columns.astype(str)]
+        # group classes and then count instances of each class
+        agg = (data.groupby('pclass')['pclass'].count()
+               .to_frame('count').reset_index())
+
+        agg_target, tit_target = self.output()
+        with agg_target.open('w') as af, tit_target.open('w') as tf:
+            agg.to_csv(af, index=False)
+            data.to_csv(tf, index=False)
+
+
+class LoadData(luigi.Task):
+    '''
+    Load source and aggregate into sql server
+    '''
+
+    def requires(self):
+        return TransformData()
+
+    def run(self):
+        agg, tit = self.input()
+        with agg.open('r') as af, tit.open('r') as tf:
+            agg_data = pd.read_csv(af)
+            source_data = pd.read_csv(tf)
+
+        conn = sqlite3.connect('database.sqlite')
+
+        try:
+            with conn:
+                agg_data.to_sql('class_agg', con=conn, if_exists='replace')
+                source_data.to_sql('titanic', con=conn, if_exists='replace')
+        except:
+            raise
+        finally:
+            conn.close()
+
+
 if __name__ == '__main__':
-    luigi.run()
+    luigi.run(['LoadData'])
