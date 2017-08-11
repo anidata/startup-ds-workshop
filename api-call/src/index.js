c52988ece1ac38f41a35da6dc4e43040d5e77a5e
Bryant Menn <bryant.menn@gmail.com>
2017-08-11 00:59:55 -0400

    Remove api call example after migrating to examples/web

--- api-call/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Printer from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Printer subreddit='reactjs'/>, document.getElementById('root'));
-registerServiceWorker();
